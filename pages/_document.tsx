3a8ef8db72b9a4c2a74dd613577e97ee2a546a72
Dion Gee <dion@wntr.com.au>
2025-02-04 00:03:20 -0700

    Final additions to the Gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../utils/analytics'
+
+const gtag = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? GA_TRACKING_ID : null
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`} />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${gtag}', { page_path: window.location.pathname });` }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
